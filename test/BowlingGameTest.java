0504f5eba70192131179c20f441fe8772b72d000
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-07-16 09:56:30 +0700

    all done

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -3,12 +3,60 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
+    private BowlingGame game;
+
+    @BeforeEach
+    void setup() {
+        game = new BowlingGame();
+    }
+
     @Test
     void testAllMissedGame() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(0, 20);
         assertEquals(0, game.score());
+    }
+
+    @Test
+    void testAllOneGame() {
+        rollMany(1, 20);
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    void testSpare() {
+        rollSpare();
+        game.roll(4);
+        rollMany(0, 17);
+        assertEquals(18, game.score());
+    }
+
+    @Test
+    void testStrike() {
+        rollStrike();
+        game.roll(1);
+        game.roll(2);
+        rollMany(0, 16);
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    void testPerfectGame() {
+        rollMany(10, 12);
+        assertEquals(300, game.score());
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
+    private void rollSpare() {
+        game.roll(3);
+        game.roll(7);
+    }
+
+    private void rollMany(int pins, int times) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
     }
 }
