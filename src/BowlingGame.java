cfa48a7d76c12e3206a073d46c7005368f8c8197
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-07-16 08:46:54 +0700

    chay test thu OK

---
+++ src/BowlingGame.java
@@ -0,0 +1,16 @@
+class BowlingGame {
+    BowlingGame game = new BowlingGame();
+
+    void roll(int pins) {
+    }
+
+    int score() {
+        return 0;
+    }
+
+    void repeat() {
+        int pins = 0;
+        game.roll(pins);
+    }
+    int score = game.score();
+}
---
+++ test/BowlingGameTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
